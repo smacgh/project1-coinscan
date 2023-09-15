27eb5378b4af89751cbbcdf0aa0238bf61fe64b7
smacgh <stevunmauk@gmail.com>
2023-09-14 22:09:22 -0500

    more notes

--- imageprocess.py
+++ imageprocess.py
@@ -3,4 +3,5 @@ import cv2
 # Check image
 #       Check for possible misprints on coin
 #       identify year
-#       Magnify image size   
+#       Magnify image size
+#       Stage1: give out % readings on potential misprints
