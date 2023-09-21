0edb5b91ab0657a67adcf60dbf91bff242ac7869
smacgh <stevunmauk@gmail.com>
2023-09-20 20:27:46 -0500

    check

--- eye2.py
+++ eye2.py
@@ -1,7 +1,7 @@
 import cv2
 import matplotlib
 # This will interface with ultrasonic sensor!
-#
+# high precision and short range
 #   Checks the surface height and gives a matrix
 #   Compare to a database of "ideal coins"
 #   Circle affected area of possible defect
